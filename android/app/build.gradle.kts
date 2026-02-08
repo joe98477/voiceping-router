fa4ddaea5c150b449380ad0d628b71d8fe2a7241
Jotham Grace - Android Studio <jotham.grace@bigpond.com>
2026-02-08 14:21:14 +1100

    fix for kotlin options

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,10 +42,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-    }
-
     buildFeatures {
         compose = true
         buildConfig = true
